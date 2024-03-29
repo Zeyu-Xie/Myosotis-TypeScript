ba3a0e3423a34a3452111366fa5317afd5921f0c
Acan <xie.zeyu20@gmail.com>
2024-03-29 10:40:23 +0800

    .

--- main.js
+++ main.js
@@ -8,9 +8,39 @@ const c = true;
 const d = null;
 const e = undefined;
 // any 类型
-let value = 64;
+let value1 = 64;
 // any 类型可以赋值为任意类型
 // 相当于暴力通过类型检查
-value = "Hello World! ";
-value = true;
-value = {};
+value1 = "Hello World! ";
+value1 = true;
+value1 = {};
+// unknown 类型
+let value2 = 10;
+value2 = 1.25;
+value2 = true;
+// unknown 类型不能赋值给 unknown 和 any 外的任何类型
+value1 = value2;
+// 当 unknown 进行了类型断言或类型推断后，就可以赋值给其他类型
+value2 = "Hello World! ";
+if (typeof value2 === "string") {
+    const str = value2.toLowerCase();
+}
+// void 类型
+function print_greetings() {
+    console.log("Hello World! ");
+}
+function withNever(foo) {
+    if (typeof foo === "string") {
+        console.log(foo.toLowerCase());
+    }
+    else if (typeof foo === "number") {
+        console.log(foo - 1);
+    }
+    else if (typeof foo === "boolean") {
+        console.log(!foo);
+    }
+    // never 类型可用于检查是否所有类型都已经处理
+    else {
+        const check = foo;
+    }
+}
--- main.ts
+++ main.ts
@@ -10,9 +10,46 @@ const e: undefined = undefined;
 
 // any 类型
 
-let value: any = 64;
+let value1: any = 64;
 // any 类型可以赋值为任意类型
 // 相当于暴力通过类型检查
-value = "Hello World! ";
-value = true;
-value = {};
+value1 = "Hello World! ";
+value1 = true;
+value1 = {};
+
+// unknown 类型
+
+let value2: unknown = 10;
+value2 = 1.25;
+value2 = true;
+// unknown 类型不能赋值给 unknown 和 any 外的任何类型
+value1 = value2;
+// 当 unknown 进行了类型断言或类型推断后，就可以赋值给其他类型
+value2 = "Hello World! ";
+if (typeof value2 === "string") {
+  const str: string = value2.toLowerCase();
+}
+
+// void 类型
+
+function print_greetings(): void {
+  console.log("Hello World! ");
+}
+
+// never 类型
+
+type Foo = string | number | boolean;
+
+function withNever(foo: Foo): void {
+  if (typeof foo === "string") {
+    console.log(foo.toLowerCase());
+  } else if (typeof foo === "number") {
+    console.log(foo - 1);
+  } else if (typeof foo === "boolean") {
+    console.log(!foo);
+  } 
+  // never 类型可用于检查是否所有类型都已经处理
+  else {
+    const check: never = foo;
+  }
+}
